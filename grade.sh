39bdb08af3149b40cc36c5007f3783bb326f6a4f
Ian <icunningham@ucsd.edu>
2023-03-01 16:17:47 -0800

    doesn't work

--- grade.sh
+++ grade.sh
@@ -1,6 +1,8 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
+rm compile-error.txt
+
 git clone $1 student-submission
 echo 'Finished cloning'
 
@@ -17,9 +19,9 @@ else
 fi 
 
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2>compile-error.txt
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java 2> compile-error.txt
 
-if [[ -e "compile-error.txt" ]]
+if [[ $? -ne 0 ]]
 then
     echo "Compile error, check compile-error.txt"
     exit 1
@@ -27,5 +29,13 @@ else
     echo "compiled"
 fi
 
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar"  org.junit.runner.JUnitCore TestListExamples > junit-errors.txt
 
+if grep -q "FAILURES!!!" junit-errors.txt
+then
+    echo "You had at least one failing test, you fail"
+    echo `grep -c "results.txt" compile-error.txt`
+    ERROR_NUMBERS = ${"error":12:2}
+    echo ERROR_NUMBERS
+
+fi
